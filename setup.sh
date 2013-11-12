0efae01ee2fa64602a3d0ee19a571dc28ac69aaa
John Lozano <jlozano@peer1.com>
2013-11-12 11:18:19 -0600

    setup.sh will install all '.x' files

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 find . -name "*.x" | while read FILENAME; do 
 	FILE=$(basename ${FILENAME%.*}); 
-	DIR="$( cd "$( dirname "${file}" )" && pwd )"
-	echo "$DIR/$FILE";
-	#eval "ln -s $FILENAME ~/$FILE";
+	DIR=$( cd "$( dirname "${FILE}" )" && pwd )
+	#echo "$DIR/$FILE";
+	eval "ln -s $DIR/$FILE.x ~/$FILE";
 done
